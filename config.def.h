5ae2d71369737abed640a6505df1ea0245be5b93
Gabriel Fiuza <gabrielmenezesfiuza@gmail.com>
2022-09-26 09:04:19 -0300

    Update Config

--- config.def.h
+++ config.def.h
@@ -11,13 +11,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        	 = 1;     /* 0 means no systray */
 static const int showbar            	 = 1;     /* 0 means no bar */
 static const int topbar             	 = 1;     /* 0 means bottom bar */
-static const char *fonts[]          	 = { "Ubuntu Nerd Font:size=12" };
-static const char dmenufont[]       	 = "Ubuntu Nerd Font:size=12";
+static const char *fonts[]          	 = { "Ubuntu Nerd Font:size=18" };
+static const char dmenufont[]       	 = "Ubuntu Nerd Font:size=18";
 static const char col_gray1[]       	 = "#222222";
 static const char col_gray2[]       	 = "#444444";
 static const char col_gray3[]       	 = "#bbbbbb";
 static const char col_gray4[]       	 = "#eeeeee";
-static const char col_cyan[]         	 = "#005577";
+static const char col_cyan[]         	 = "#222222";
 static const char *colors[][3]      	 = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -66,8 +66,20 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+static const char *flameshotcmd[]  = { "flatpak", "run", "org.flameshot.Flameshot", "gui", NULL };
+static const char *volume[]  = { "/home/gabrielfiuza/.config/rofi/volume.sh", NULL };
+static const char *mpd[]  = { "/home/gabrielfiuza/.config/rofi/mpd.sh", NULL };
+static const char *links[]  = { "/home/gabrielfiuza/.config/rofi/quicklinks.sh", NULL };
+static const char *apps[]  = { "/home/gabrielfiuza/.config/rofi/apps.sh", NULL };
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flameshotcmd } },
+	{ MODKEY,                   	XK_v,      spawn,          {.v = volume } },
+	{ MODKEY|ShiftMask,             XK_v,      spawn,          {.v = mpd } },
+	{ MODKEY,                   	XK_e,      spawn,          {.v = apps } },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = links } },
+	
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
